abcbed0858c3076073a4e3c6df499f0eb5c82463
William Leighton Dawson <wld224@nyu.edu>
2016-04-24 05:35:11 +0400

    implemented driver registration and tested thoroughly

--- server/index.js
+++ server/index.js
@@ -1,41 +1,83 @@
-//Lets require/import the HTTP module
+// Import the HTTP module
 var http = require('http');
 
-//Lets define a port we want to listen to
+// Define the port to listen to (80 is the standard for http:// over IP)
 const PORT=80; 
 
-//URL decoder
+// List of drivers
+var drivers = [];
+
+// URL decoder
 function decode(str) {
-     return unescape(str.replace(/\_/g, " "));
+     return unescape(str.replace(/\_/g, ' '));
 }
 
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    var info = {};
-    var raw_data = decode(request.url.slice(1));
-    var data_list = raw_data.split("|");
-    if (data_list.length == 5) {
-        response.write("Parsed input line for line (separated by '|' characters):\n", 'ascii');
-        info['type']    = data_list[0];
-        info['lat']     = data_list[1];
-        info['lon']     = data_list[2];
-        info['phone']   = data_list[3];
-        info['message'] = data_list[4];
-        for (var key in info) {
-            var tmp_str = key + "\t" + info[key] + "\n";
-            response.write(tmp_str, 'ascii');
+function registerDriver(lat, lon, phone, name) {
+    var new_driver = {};
+    var exists = false;
+    new_driver['lat']   = lat;
+    new_driver['lon']   = lon;
+    new_driver['phone'] = phone;
+    new_driver['name']  = name;
+    console.log("Incoming phone number: " + phone);
+    for (var i = 0; i < drivers.length && !exists; i++) {
+        var old_driver_str = drivers[i];
+        var old_driver = JSON.parse(old_driver_str);
+        //console.log("Checking against: " + old_driver['phone']);
+        if (old_driver['phone'] == new_driver['phone']) {
+            exists = true;
+            old_driver['lat'] = new_driver['lat'];
+            old_driver['lon'] = new_driver['lon'];
+            old_driver['name'] = new_driver['name'];
+            console.log('updateDriver');
         }
-    } else {
-        response.write("ERROR: Incorrect number of fields.\n");
     }
+    if (!exists) {
+        drivers.push(JSON.stringify(new_driver));
+        console.log('registerDriver');
+    }
+    console.log('\n');
+}
+
+// A handler so that the main handleRequest function can be swapped out with the SMS functionality easily
+function handleList(data_list) {
+    var info = {};
+    info['type']    = data_list[0];
+    info['lat']     = data_list[1];
+    info['lon']     = data_list[2];
+    info['phone']   = data_list[3];
+    info['message'] = data_list[4];
+    if (info['type'] == 'S' || info['type'] == 'U') {
+        registerDriver(info['lat'], info['lon'], info['phone'], info['message']); // for now both go to the same function
+    }
+    // This response is for debugging purposes and will eventually change.
+    var response_text = "Parsed input line for line (separated by '|' characters):\n";
+    for (var key in info) {
+        var tmp_str = key + '\t' + info[key] + '\n';
+        response_text += tmp_str;
+    }
+    return response_text;
+}
+
+// The function which handles requests and send response
+function handleRequest(request, response){
+    var raw_data = decode(request.url.slice(1));
+    var data_list = raw_data.split('|');
+    var response_text = '';
+    if (data_list.length == 5) {
+        response_text = handleList(data_list);
+    } else {
+        response_text = 'ERROR: Incorrect number of fields.\n';
+    }
+    response.write(response_text, 'ascii');
     response.end();
 }
 
-//Create a server
+//Create the server
 var server = http.createServer(handleRequest);
 
-//Lets start our server
+// Start the server
 server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+    // Triggered when server is listening.
+    console.log('Server listening on: http://localhost:%s', PORT);
 });
