6f12834d8a921229d2f1ccafadde175cbbc27e23
gc1569 <gc1569@nyu.edu>
2016-05-09 10:51:55 +0400

    sms demo

--- app/app/src/main/java/com/example/student/ctexiv1/DriverActivities/DriverWaitActivity.java
+++ app/app/src/main/java/com/example/student/ctexiv1/DriverActivities/DriverWaitActivity.java
@@ -6,23 +6,19 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.android.volley.Request;
 
 import android.content.Context;
 import android.widget.Toast;
 
 import com.example.student.ctexiv1.R;
-import com.example.student.ctexiv1.Utils.LastLocationProvider;
-import com.example.student.ctexiv1.Utils.LocationActivity;
+import com.example.student.ctexiv1.Utils.LocationSMSActivity;
 import com.example.student.ctexiv1.Utils.RequestSingleton;
 import com.example.student.ctexiv1.Utils.ServerCallback;
 
 import android.location.LocationListener;
 
-public class DriverWaitActivity extends LocationActivity {
+public class DriverWaitActivity extends LocationSMSActivity {
 
     private double longitudeGPS;
     private double latitudeGPS;
@@ -45,9 +41,12 @@ public class DriverWaitActivity extends LocationActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(DriverWaitActivity.this, "GPS Provider update", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(DriverWaitActivity.this, "GPS Provider update" + longitudeGPS + latitudeGPS, Toast.LENGTH_SHORT).show();
                         final Intent i = new Intent(context, DriverSecondActivity.class);
-                        RequestSingleton.getInstance(context).addToGETRequestQueue("S|0.834|28.577|0097126352855|Chimamanda_Adichie|anyad", new ServerCallback() {
+                        send
+
+                        /*
+                        RequestSingleton.getInstance(context).addToGETRequestQueue("S|" + latitudeGPS + "|" + latitudeGPS+ "|" + getIntent().getStringExtra("PassedNumber")+"||"+ getIntent().getStringExtra("PassedName"), new ServerCallback() {
                             @Override
                             public void onSuccess(String result) {
 
@@ -59,10 +58,11 @@ public class DriverWaitActivity extends LocationActivity {
                                 i.putExtra("PassedLat", parsedData[1]);
                                 i.putExtra("PassedLong", parsedData[2]);
 
-                                //startActivity(i);
-                                //finish();
+                                startActivity(i);
+                                finish();
                             }
                         });
+                        */
                     }
                 });
             }
